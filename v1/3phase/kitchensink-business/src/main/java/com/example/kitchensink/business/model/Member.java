ba85153a11014eedebb876fb626283eb0ef6f6e8
Gian Paolo Santopaolo <gianpaolo.santopaolo@gmail.com>
2025-05-09 09:56:02 +0200

    some work

--- v1/3phase/kitchensink-business/src/main/java/com/example/kitchensink/business/model/Member.java
+++ v1/3phase/kitchensink-business/src/main/java/com/example/kitchensink/business/model/Member.java
@@ -1,23 +1,40 @@
 package com.example.kitchensink.business.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member {
+
     @Id @GeneratedValue
     private Long id;
 
-    @NotNull @Size(min = 1, max = 25)
-    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
-
-    @NotNull @Email @NotEmpty
     private String email;
 
-    @NotNull @Size(min = 10, max = 12)
-    private String phoneNumber;
+    public Member() { }
 
-    // getters and setters omitted for brevity
+    // ------ ADD THESE ------
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
